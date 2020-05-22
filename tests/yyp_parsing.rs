17fb4ccb368fdc3c617ee43c4bc4be41d654a2f6
Jonathan Spira <jjspira@gmail.com>
2020-05-22 01:01:54 -0700

    added various tests....yyp parsing looks solid.

--- tests/yyp_parsing.rs
+++ tests/yyp_parsing.rs
@@ -1,10 +1,20 @@
+use include_dir::{include_dir, Dir, DirEntry};
 use yy_boss::{utils::TrailingCommaUtility, yy_typings::Yyp};
 
 #[test]
 fn trivial_yyp_parse() {
+    let all_sprites: Dir = include_dir!("tests/examples/yyp_examples");
     let mut tcu = TrailingCommaUtility::new();
-    let yyp_example =
-        tcu.clear_trailing_comma(include_str!("./examples/yyp_examples/yyp_test0.yyp"));
+    
 
-    let _: Yyp = serde_json::from_str(&yyp_example).unwrap();
+    for sprite_file in all_sprites.find("**/*.yyp").unwrap() {
+        match sprite_file {
+            DirEntry::File(file) => {
+                let our_str = std::str::from_utf8(file.contents()).unwrap();
+                let our_str = tcu.clear_trailing_comma(our_str);
+                let _: Yyp = serde_json::from_str(&our_str).unwrap();
+            }
+            _ => {}
+        }
+    }
 }
