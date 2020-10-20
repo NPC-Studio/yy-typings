c66ad0083c1dde1a6935ea15d1706a6689287e57
Jonathan Spira <jjspira@gmail.com>
2020-10-20 10:04:00 -0700

    allow for override property

--- src/typings/object_yy/object_constants.rs
+++ src/typings/object_yy/object_constants.rs
@@ -1,23 +1,31 @@
 use serde::{Deserialize, Serialize};
 use smart_default::SmartDefault;
 
-#[derive(Debug, Copy, Serialize, Deserialize, SmartDefault, PartialEq, Eq, Clone, Ord, PartialOrd)]
+#[derive(
+    Debug, Copy, Serialize, Deserialize, SmartDefault, PartialEq, Eq, Clone, Ord, PartialOrd,
+)]
 pub enum ConstGmObject {
     #[serde(rename = "GMObject")]
     #[default]
     Const,
 }
 
-#[derive(Debug, Copy, Serialize, Deserialize, SmartDefault, PartialEq, Eq, Clone, Ord, PartialOrd)]
+#[derive(
+    Debug, Copy, Serialize, Deserialize, SmartDefault, PartialEq, Eq, Clone, Ord, PartialOrd,
+)]
 pub enum ConstGmEvent {
     #[serde(rename = "GMEvent")]
     #[default]
     Const,
 }
 
-#[derive(Debug, Copy, Serialize, Deserialize, SmartDefault, PartialEq, Eq, Clone, Ord, PartialOrd)]
+#[derive(
+    Debug, Copy, Serialize, Deserialize, SmartDefault, PartialEq, Eq, Clone, Ord, PartialOrd,
+)]
 pub enum ConstGmObjectProperty {
     #[serde(rename = "GMObjectProperty")]
     #[default]
     Const,
+    #[serde(rename = "GMOverriddenProperty")]
+    Override,
 }
